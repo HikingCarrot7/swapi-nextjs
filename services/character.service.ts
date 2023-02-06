503fad6e83e05dd6fc62aafff93b8484d3226d96
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-02-06 17:16:44 -0600

    Added adicional methods to get results as dto in character service

--- services/character.service.ts
+++ services/character.service.ts
@@ -1,11 +1,24 @@
-import { PaginatedCharactersResult } from '@models/PaginatedCharactersResult';
+import { CharacterDTO } from '@models/dtos/CharacterDTO';
+import { PaginatedCharactersDTO } from '@models/dtos/PaginatedCharactersDTO';
+import { paginatedDtoToEntity } from '@models/mappers/characterMapper';
+import { PaginatedCharacters } from '@models/PaginatedCharacters';
 import api from '../config/axios';
 
 export const getPaginatedCharacters = async (
   page: number,
-): Promise<PaginatedCharactersResult> => {
-  const result = await api.get<PaginatedCharactersResult>(
-    `/people?page=${page}&format=json`,
-  );
+): Promise<PaginatedCharacters> => {
+  const result = await getPaginatedCharactersDTO(page);
+  return paginatedDtoToEntity(result);
+};
+
+export const getPaginatedCharactersDTO = async (page: number) => {
+  const result = await api.get<PaginatedCharactersDTO>(`/people?page=${page}`);
+  return result.data;
+};
+
+export const getCharacterDTO = async (
+  characterId: number,
+): Promise<CharacterDTO> => {
+  const result = await api.get<CharacterDTO>(`/people/${characterId}`);
   return result.data;
 };
