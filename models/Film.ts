bc912c690a508505e9ae54423b36521cd2e09642
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-02-06 17:17:43 -0600

    Added Film and FilmDTO models

---
+++ models/Film.ts
@@ -0,0 +1,9 @@
+export interface Film {
+  id: number;
+  title: string;
+  episodeId: number;
+  openingCrawl: string;
+  director: string;
+  producer: string;
+  releaseDate: string;
+}
---
+++ models/dtos/FilmDTO.ts
@@ -0,0 +1,10 @@
+export interface FilmDTO {
+  title: string;
+  episode_id: number;
+  opening_crawl: string;
+  director: string;
+  producer: string;
+  release_date: string;
+  characters: string[];
+  url: string;
+}
